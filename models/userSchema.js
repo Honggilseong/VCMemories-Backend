6a4b7f85a7eb6fc941b809f61c1e7b7ea8f5a01b
unknown <hong.gilseong@gmail.com>
2022-06-07 19:32:24 +0900

    유저 스키마 작성

---
+++ models/userSchema.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  id: { type: String },
+  pic: {
+    type: String,
+    default:
+      "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
+
+export default mongoose.model("User", userSchema);
