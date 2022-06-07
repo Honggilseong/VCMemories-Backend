eb95645221dc91868950255649633b2c15171970
unknown <hong.gilseong@gmail.com>
2022-06-07 20:35:53 +0900

    유저 루트 작성

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+
 const app = express();
 dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
