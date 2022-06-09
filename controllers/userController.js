d01954f636adadb473fa0d5cdefd69835c23ec0a
unknown <hong.gilseong@gmail.com>
2022-06-09 17:53:48 +0900

    로그인 기능 작성

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/userSchema.js";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 export const signup = async (req, res) => {
   const { email, password, name, profilePicture } = req.body;
@@ -39,5 +40,39 @@ export const signup = async (req, res) => {
     res.status(200).json({ user: userData });
   } catch (error) {
     res.status(404).json({ message: error.message });
+  }
+};
+
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (!existingUser)
+      return res.status(404).json({ message: "User doesn't exist." });
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      "test",
+      { expiresIn: "30d" }
+    );
+
+    const userData = {
+      email: existingUser.email,
+      name: existingUser.name,
+      profilePicture: existingUser.profilePicture,
+      token,
+    };
+
+    res.status(200).json({ user: userData });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
   }
 };
