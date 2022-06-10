f3c35a09809aa2e64aa276d69587e0a461fcf04d
unknown <hong.gilseong@gmail.com>
2022-06-10 23:20:08 +0900

    유저 토큰 확인하는 미들웨어 작성

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { createPost } from "../controllers/postsControllers.js";
+import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", createPost);
+router.post("/", protect, createPost);
 
 export default router;
