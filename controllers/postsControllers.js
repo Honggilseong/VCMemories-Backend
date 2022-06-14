2a254884079a95235f707c1398851ecf3792184d
unknown <hong.gilseong@gmail.com>
2022-06-14 16:12:30 +0900

    작성된 유저 포스트 가져오기 작성

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -11,3 +11,13 @@ export const createPost = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await Post.find();
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createPost } from "../controllers/postsControllers.js";
+import { createPost, getPosts } from "../controllers/postsControllers.js";
 import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+router.get("/", protect, getPosts);
 router.post("/createpost", protect, createPost);
 
 export default router;
