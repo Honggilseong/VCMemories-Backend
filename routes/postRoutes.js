054678276448670fc15ca8a74c5f2ab882da53be
unknown <hong.gilseong@gmail.com>
2022-06-14 15:35:43 +0900

    createpost 루트 설정

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,6 +4,6 @@ import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", protect, createPost);
+router.post("/createpost", protect, createPost);
 
 export default router;
