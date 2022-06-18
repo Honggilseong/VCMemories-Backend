bfd5d656d2e74fd90e700dc4f8e79b0bf56f7e6e
unknown <hong.gilseong@gmail.com>
2022-06-18 16:04:58 +0900

    유저 정보 루트 작성

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signin, signup } from "../controllers/userController.js";
+import { getUserInfo, signin, signup } from "../controllers/userController.js";
+import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/signin", signin);
 router.post("/signup", signup);
-
+router.get("/getuserinfo/:id", protect, getUserInfo);
 export default router;
