fb2651568eb9480e7ce1815c10c86d7b16eee0de
unknown <hong.gilseong@gmail.com>
2022-06-21 15:09:25 +0900

    게시글 좋아요 루트 작성

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,6 +3,7 @@ import {
   createPost,
   deletePost,
   getPosts,
+  likePost,
 } from "../controllers/postsControllers.js";
 import protect from "../middleware/authMiddleware.js";
 
@@ -11,4 +12,5 @@ const router = express.Router();
 router.get("/", protect, getPosts);
 router.post("/createpost", protect, createPost);
 router.delete("/:id", protect, deletePost);
+router.patch("/:id/likepost", protect, likePost);
 export default router;
