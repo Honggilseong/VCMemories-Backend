3c07d682dde13c33861693edefa80004fe670665
unknown <hong.gilseong@gmail.com>
2022-07-11 14:37:44 +0900

    notification 루트 작성

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,7 @@ import {
   getAllUsers,
   getSearchingUser,
   getUserInfo,
+  sendNotification,
   signin,
   signup,
   uploadProfileImage,
@@ -16,6 +17,7 @@ router.post("/signin", signin);
 router.post("/signup", signup);
 router.patch("/followuser/:id", protect, followUser);
 router.patch("/uploadprofileimage", protect, uploadProfileImage);
+router.patch("/:id/notification", protect, sendNotification);
 router.get("/getuserinfo/:id", protect, getUserInfo);
 router.get("/search/:username", protect, getSearchingUser);
 router.get("/getallusers", protect, getAllUsers);
