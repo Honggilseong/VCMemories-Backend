321a277e2ead050272315a972f1915511a0e6f5a
unknown <hong.gilseong@gmail.com>
2022-07-16 20:24:04 +0900

    알림을 지울 수 있게 api 주소 작성

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  deleteNotifications,
   followUser,
   getAllUsers,
   getSearchingUser,
@@ -20,6 +21,7 @@ router.patch("/followuser/:id", protect, followUser);
 router.patch("/uploadprofileimage", protect, uploadProfileImage);
 router.patch("/:id/notification", protect, sendNotification);
 router.patch("/:id/readnotification", protect, readNotification);
+router.patch("/:id/deletenotifications", protect, deleteNotifications);
 router.get("/getuserinfo/:id", protect, getUserInfo);
 router.get("/search/:username", protect, getSearchingUser);
 router.get("/getallusers", protect, getAllUsers);
