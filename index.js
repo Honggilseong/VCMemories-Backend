37ef70144293669757ee794b7c552dcfcdae2345
unknown <hong.gilseong@gmail.com>
2022-08-02 22:02:57 +0900

    package에 engines 추가

--- index.js
+++ index.js
@@ -23,14 +23,14 @@ app.use("/user", userRoutes);
 app.use("/posts", postsRoutes);
 app.use("/report", reportRoutes);
 
+const PORT = process.env.PORT || 5000;
+
 mongoose
   .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() =>
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port: ${PORT}`)
-    )
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
   .catch((error) => console.log(error.message));
