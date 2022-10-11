c5cdc90fd87642c4dad6e06d9ec3ad26102ae8ba
unknown <hong.gilseong@gmail.com>
2022-10-11 14:16:44 +0900

    pagination route 작성 게시글 수정할 수 있는 route 작성

--- routes/boardRoutes.js
+++ routes/boardRoutes.js
@@ -8,12 +8,15 @@ import {
   editBoardPostReply,
   getAllBoardPosts,
   getBoardPost,
+  getBoardPostsQuery,
+  getEditBoardPost,
   getUserBoardPostList,
   leaveBoardPostComment,
   leaveBoardPostReply,
   likeBoardPost,
   likeBoardPostComment,
   likeBoardPostReply,
+  updateBoardPost,
   uploadBoardPostImage,
 } from "../controllers/boardControllers.js";
 import protect from "../middleware/authMiddleware.js";
@@ -22,9 +25,12 @@ const router = express.Router();
 
 router.get("/getallboardposts", getAllBoardPosts);
 router.get("/getboardpost/:id", getBoardPost);
+router.get("/geteditboardpost/:id", protect, getEditBoardPost);
 router.get("/getuserboardpostlist/:username", getUserBoardPostList);
+router.get("/vrchat", getBoardPostsQuery);
 router.post("/boardpost/post", createBoardPost);
 router.post("/uploadboardpostpreviewimage/:id", protect, uploadBoardPostImage);
+router.patch("/updateboardpost/:id", protect, updateBoardPost);
 router.patch("/leaveboardpostcomment/:id", protect, leaveBoardPostComment);
 router.patch("/leaveboardpostreply/:id", protect, leaveBoardPostReply);
 router.patch("/likeboardpostcomment/:id", protect, likeBoardPostComment);
